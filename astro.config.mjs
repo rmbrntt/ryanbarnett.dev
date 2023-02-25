6c3eac2334caa2e8a00ae447f37a90260ab6ca8c
Ryan Barnett <ryan@barnett.io>
2023-02-25 15:16:14 -0500

    feat: add skills, update nav, add github pages workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,5 +8,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  site: 'ryanbarnett.dev',
 });
