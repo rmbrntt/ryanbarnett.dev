918c10dfdfff1eb891b4300d040d6c5ae7c9f22b
Ryan Barnett <ryan@barnett.io>
2023-02-25 19:53:06 -0500

    fix: abide by astro's protocol for content naming conventions

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,10 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 // https://astro.build/config
+import compress from "astro-compress";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  site: 'https://ryanbarnett.dev',
+  integrations: [tailwind(), react(), compress()],
+  site: 'https://ryanbarnett.dev'
 });
